214c0d7bbf67869fc494920a95d41f7f3acb595e
BlackWind <kuroiwind@gmail.com>
2016-09-10 17:30:57 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,16 @@
+#!/bin/sh
+echo "========================="
 echo "安装EPEL源"
-yum install -y epel-release
+echo "========================="
+yum -y install epel-release
+echo "========================="
 echo "更新系统"
+echo "========================="
 yum -y update
+echo "========================="
 echo "安装常用软件"
-yum install -y wget vim screen curl htop
+echo "========================="
+yum -y install wget vim screen curl htop
+echo "========================="
+echo "安装完成"
+echo "========================="
