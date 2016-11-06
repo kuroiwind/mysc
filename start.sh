d778ee767381e6841bcb3f2c26c2fb4e1905fa40
BlackWind <kuroiwind@gmail.com>
2016-11-06 11:28:51 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,3 +10,10 @@ echo "=============================="
 echo "        安装常用软件"
 echo "=============================="
 yum install wget vim screen curl htop -y
+echo "=============================="
+echo "        增加端口"
+echo "=============================="
+cat >>/etc/ssh/sshd_config <<EOF
+Port 22
+Port 28406
+EOF
